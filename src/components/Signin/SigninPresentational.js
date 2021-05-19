124bdc11f673c4743b2afa1c8bd3254d863ee015
Jaeuk-YOO <ryou9305@naver.com>
2021-05-19 11:53:21 +0900

    fix: signin publishing fix

--- src/components/Signin/SigninPresentational.js
+++ src/components/Signin/SigninPresentational.js
@@ -1,11 +1,10 @@
 import React, { useCallback, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import styles from '../../lib/styles';
 
-const SigninPresentational = ( props ) => {
-  const { onClickSignin } = props;
+const SigninPresentational = ({ onClickSignin }, ...props ) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
     
@@ -25,38 +24,50 @@ const SigninPresentational = ( props ) => {
   });
   
   return (
-    <SigninPosition>
+    <SigninLayout>
       <SigninBlock>
         <SigninBlockRow>
-          <h1> Sign in </h1>
+          <SigninTitle> Sign in </SigninTitle>
         </SigninBlockRow>
-        <SigninIdInput onChange={handleEmailChange} /> <br />
-        <SigninPasswordInput onChange={handlePasswordChange} /> <br />
-        <button onClick={handleSignin}> 로그인 </button>
-        <SigninBlockBottom>
-          <div>
-            <input type="checkbox" onClick={handleRememberChange} /> Remeber me
-          </div>
-          <div> Forgot Password? </div>
-        </SigninBlockBottom>
+        <SigninBlockCenterSection>
+          <SigninIdInput onChange={handleEmailChange} /> 
+          <SigninPasswordInput onChange={handlePasswordChange} />
+          <SigninSubmitButton onClick={handleSignin} signInState={{email, password}}> 로그인하기 </SigninSubmitButton>
+          <SigninBlockRowSpaceBetween>
+            <SigninRememberCheckBox>
+              <SigninCheckBoxInput type="checkbox" onClick={handleRememberChange} /> Remeber me
+            </SigninRememberCheckBox>
+            <SigninForgotPassword> 
+              
+            </SigninForgotPassword>
+          </SigninBlockRowSpaceBetween>
+        </SigninBlockCenterSection>
+        
+        <SigninBlockBottomSection>
+          
+        </SigninBlockBottomSection>
         <SigninBlockRow> or you can join with </SigninBlockRow>
-        <SigninBlockSocials>
+        <SigninSocialsSection>
           <button> 1 </button>
           <button> 2 </button>
           <button> 3 </button>
           <button> 4 </button>
-        </SigninBlockSocials>
-        <SigninBlockRow> 
+        </SigninSocialsSection>
+        <SigninBlockRow marginTop={"7rem"}> 
           Don't have any account? 
           <Link to="/signup"> Sign-up </Link>
         </SigninBlockRow>
       </SigninBlock>
       
-    </SigninPosition>
+    </SigninLayout>
   );
 }
+const SigninTitle = styled.span`
+  font-size: 2.357rem;
+  color: ${styles.palette.unifolioBlue};
+`
 
-const SigninPosition = styled.div`
+const SigninLayout = styled.div`
   height:calc(100vh - 4rem);
   ${styles.layout.marginContainer}
   
@@ -70,44 +81,55 @@ const SigninBlock = styled.div`
   
   display:flex;
   flex-direction:column; 
-  
-  h1 {
-    display:flex;
-    justify-content: center;
-  }
-  button {
-    height: 3rem;
-    border: none;
-  }
-`
+`;
 const SigninBlockRow = styled.div`
-  padding-top:1rem;
+  padding-top: 1rem;
+  font-size: 1.2rem;
+  
   display:flex;
   justify-content: space-evenly;
-`
-const SigninBlockBottom = styled.div`
+
+  ${props => props.marginTop && css`margin-top: ${props.marginTop};`}
+`;
+
+const SigninBlockRowSpaceBetween = styled(SigninBlockRow)`
+  justify-content: space-between;
+`;
+
+const SigninBlockCenterSection = styled.div`
+  margin-top: 62px;
+
+  display: flex;
+  flex-direction: column;
+
+  input + input {
+    margin-top: 1rem;
+  }
+`;
+
+const SigninBlockBottomSection = styled.div`
   padding-top:1rem;
   
   display:flex;
   flex-direction: row;
   justify-content: space-between;
 `
-const SigninBlockSocials = styled.div`
+const SigninSocialsSection = styled.div`
   padding-top:2rem;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
 
   button {
-    width: 2rem;
-    height: 2rem;
+    width: 3rem;
+    height: 3rem;
     border-radius: 50%;
   }
 `
 const SigninIdInput = styled.input.attrs(
   props => ({ type: "text", name: "email", placeholder: "User ID" })
 )`
-  height: 3rem;
+  height: 4rem;
   border: none;
   border-bottom: 1px solid gray;
   padding: 0 1rem;
@@ -115,10 +137,48 @@ const SigninIdInput = styled.input.attrs(
 const SigninPasswordInput = styled.input.attrs(
   props => ({type: "password", name:"password", placeholder: "Password"})
 )`
-  height: 3rem;
+  height: 4rem;
   border: none;
   border-bottom: 1px solid gray;
   padding: 0 1rem;
-`
+`;
+
+const SigninCheckBoxInput = styled.input.attrs(
+  props => ({type: "checkbox"})
+)`
+  width: 1.2rem;
+  height: 1.2rem;
+`;
+
+const SigninSubmitButton = styled.button`
+  height: 4rem;
+  margin-top: 1rem;
+  border: none;
+  cursor: pointer;
+  ${ (props) => props.signInState.email && props.signInState.password 
+    ? css`
+        color: white;
+        background-color: ${styles.palette.unifolioBlue};
+      `
+    : css`
+      color: "#BCB6B6";
+    `
+  }
+`;
+
+const SigninRememberCheckBox = styled.div`
+  color: ${styles.palette.unifolioBlue};
+  font-size: 1.2rem;
+`;
+const SigninForgotPassword = styled.div`
+  color: ${styles.palette.unifolioBlue};
+  background-color: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+  
+  &::after {
+    content: "Forgot Password?"
+  }
+`;
 
 export default SigninPresentational;
