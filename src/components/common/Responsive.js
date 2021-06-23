bb0e3a2ab34d5ca56fb0dc08030cb6d41380fbf3
NaLeum <wognsdl@likelion.org>
2021-06-23 16:10:02 +0900

    style: Responsive 에 padding 추가

--- src/components/common/Responsive.js
+++ src/components/common/Responsive.js
@@ -6,21 +6,22 @@ const ResponsiveBlock = styled.div`
 
   max-width:100%;
   margin:0 auto;
-  @media(max-width: 768px){
+  padding: 0 10px;
+  @media(max-width: 788px){
     width: 100%;
   }
   ${(props)=>{
     switch (props.level){
       case 1:
         return css`
-          max-width:1440px;
-          @media(max-width: 1440px){
+          max-width:1460px;
+          @media(max-width: 1460px){
               width: 100%;
   }`
       case 2:
         return css`
-          max-width:1010px;
-          @media(max-width: 1010px){
+          max-width:1030px;
+          @media(max-width: 1030px){
               width: 100%;
 
   }`
