0cb52f0af96c4fce22af9c66df8cd8c62a4d522b
Jaeuk-YOO <ryou9305@naver.com>
2021-07-11 19:26:38 +0900

    fix: 프로필 헤더 작동프로세스 및 스타일 수정

--- src/components/Header/ProfileHeader.js
+++ src/components/Header/ProfileHeader.js
@@ -1,37 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import styled from 'styled-components';
-import Responsive from '../common/Responsive.js';
+import Responsive from 'components/common/Responsive.js';
 
-import palette from '../../lib/styles/palette.js';
+import palette from 'lib/styles/palette.js';
 
 const ProfileHeader = ({ history, current, submitChangeHeaderStatus }) => {
 
-	useEffect(() => {
-		document.querySelector('.active').className = 'profileheader-item';
-		if (current === undefined) {
-			document.querySelector('.profileheader-item').className += ' active';
-		} else {
-			document.querySelectorAll(`.profileheader-item[data-location=${current}]`)[0].className += ' active';
-		}
-	});
+  const [whichActive, setWhichActive] = useState('profile');
+	
 	const onClickHeaderItem = (value) => {
 		// history.push(`/my?mode=${e.target.dataset.location}`);
-		submitChangeHeaderStatus(value);
+		setWhichActive(value);
+    submitChangeHeaderStatus(value);
 	};
+  
 	return (
 		<ProfileHeaderPosition className="ProfileHeader">
 			<ProfileHeaderLayout>
 				<HeaderLayer
 					onClick={() => {onClickHeaderItem('profile'); }}
-					className="profileheader-item active"
+					className={`profileheader-item ${whichActive === 'profile' && 'active' }`}
 				>
 					프로필
 				</HeaderLayer>
 				<HeaderLayer
 					onClick={() => { onClickHeaderItem('create-association'); }}
-					className="profileheader-item"
+					className={`profileheader-item ${whichActive === 'create-association' && 'active' }`}
 				>
 					신규 투자조합 만들기
 				</HeaderLayer>
