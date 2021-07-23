9dd7662787300e878d9a67b5755d5847395104c5
NaLeum <wognsdl@likelion.org>
2021-07-23 18:33:33 +0900

    feat: 조합관리 공통 헤더

---
+++ src/components/union-manage/ManageHeader.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import {ReactComponent as LeftArrow} from "../../assets/svgs/LeftArrow.svg";
+
+const ManageHeader = ({title,backPage}) => {
+    return(
+        <Header>
+            <BackButton>
+                <LeftArrow/>
+                <span>{backPage}</span>
+            </BackButton>
+            <Title>{title}</Title>
+            <JoinButton>조합 참여 요청</JoinButton>
+        </Header>
+    )
+}
+
+export default ManageHeader;
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 70px;
+`;
+const BackButton = styled.div`
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 23px;
+    &>span{
+        margin-left: 17px;
+    }
+`;
+const Title = styled.h1`
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    margin-bottom: 0;
+`;
+const JoinButton = styled.button`
+    cursor: pointer;
+    background: #3C2FF2;
+    border-radius: 4px;
+    padding: 10px 15px;
+    color:#fff;
+    font-weight: bold;
+    border:0;
+`;
