7a9772f149e0ae43e7e3c8e3933b94c3569e05d9
ppamppamman <ryou9305@naver.com>
2021-07-25 23:13:42 +0900

    add: build directory to release branch 210725

---
+++ build/precache-manifest.d73be9b4858ddd0e8957a6a17a0361e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f329e382e5f22cab69b5c940f7e68d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9404d885a748a1fa801d",
+    "url": "/static/css/2.048727f4.chunk.css"
+  },
+  {
+    "revision": "27511579a1e9c04b3241",
+    "url": "/static/css/main.386079ed.chunk.css"
+  },
+  {
+    "revision": "9404d885a748a1fa801d",
+    "url": "/static/js/2.1eea9f2f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1eea9f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27511579a1e9c04b3241",
+    "url": "/static/js/main.56f71517.chunk.js"
+  },
+  {
+    "revision": "88ee998bdca39c891ac7",
+    "url": "/static/js/runtime-main.df3ad0ce.js"
+  },
+  {
+    "revision": "a23a06037f709086e9d45b3d3a9e170d",
+    "url": "/static/media/BottomArrow-S.a23a0603.svg"
+  },
+  {
+    "revision": "6a7f48af8b0234375e8034a84115a090",
+    "url": "/static/media/BottomArrow.6a7f48af.svg"
+  },
+  {
+    "revision": "6de1d215ad513f3f26ebe50e51ad6da1",
+    "url": "/static/media/LeftArrow.6de1d215.svg"
+  },
+  {
+    "revision": "9d0ca04809436fec47a14a7a2e86ede1",
+    "url": "/static/media/UpArrow-S.9d0ca048.svg"
+  },
+  {
+    "revision": "1c2f7fba1b6a8bf1f2ea2d5d209572d7",
+    "url": "/static/media/X.1c2f7fba.svg"
+  },
+  {
+    "revision": "43b7574fc2e1bebe9186e2eb5ac97437",
+    "url": "/static/media/ping.43b7574f.png"
+  }
+]);
