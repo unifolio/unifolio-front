925f0850805ce187549bed86ae83a4450a2d5fb8
ppamppamman <ryou9305@naver.com>
2021-10-30 09:03:06 +0900

    fix: 추가 정보 컴포넌트 추가에 따른 컨테이너 로직 수정

--- src/containers/AdditionalInfoContainer.js
+++ src/containers/AdditionalInfoContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import AdditionalInfo from 'composition/Profile/AdditionalInfo';
+import AdditionalInfoBusiness from 'composition/Profile/AdditionalInfoBusiness';
+
 import API from 'lib/api';
 
 const AdditionalInfoContainer = ({ user }) => {
@@ -10,7 +12,7 @@ const AdditionalInfoContainer = ({ user }) => {
       const result = await API.patch.userBusiness(user.id, formData);
       if (result.status === 200) {
         alert("업데이트가 완료되었습니다.");
-        window.location.reload(); // for test
+        // window.location.reload(); // for test
       }
     }
     if (user.role === "general") {
@@ -23,7 +25,12 @@ const AdditionalInfoContainer = ({ user }) => {
 	};
 
   if (!user) return <></>;
-	return <AdditionalInfo user={user} handleSubmit={handleSubmit}/>;
+  return (
+    <>
+      {user.role === "general" && <AdditionalInfo user={user} handleSubmit={handleSubmit}/>}
+      {user.role === "business" && <AdditionalInfoBusiness user={user} handleSubmit={handleSubmit}/>}
+    </>
+  );
 };
 
 export default AdditionalInfoContainer;
