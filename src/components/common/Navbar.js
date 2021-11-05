44f939b49c150cc536cd94e1bd8d2bc15d8e5516
ppamppamman <ryou9305@naver.com>
2021-11-06 06:37:48 +0900

    fix: useParams를 통한 내비바 수정

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png'
 
 const Navbar = () => {
+  const { page } = useParams();
   const isLogin = () => {
     const accessToken = localStorage.getItem('unifolioAccess');
       if (!accessToken) return false;
