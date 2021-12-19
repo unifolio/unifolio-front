4a5a0e0c9d9ffd023a44daed9ee44e8cb9a2562e
ppamppamman <ryou9305@naver.com>
2021-12-19 09:15:52 +0900

    fix: 유니언 데이터 관리 페이지 개선

--- src/pages/AssociationManagePage.js
+++ src/pages/AssociationManagePage.js
@@ -1,18 +1,28 @@
-import Responsive from 'components/common/Responsive';
 import React, { useEffect, useState } from 'react';
+
+import Responsive from 'components/common/Responsive';
 import UnionInfo from "components/Union-manage/UnionInfo";
 import ParticipationList from 'components/Union-manage/participation/ParticipationList';
 import NoticeList from 'components/Union-manage/notice/NoticeList';
+
+import useFetchUserToken from 'hooks/useFetchUserToken';
+
 import API from 'lib/api';
 
 const AssociationManagePage = () => {
+  
   const [unionData, setUnionData] = useState("");
   const [postData, setPostData] = useState("");
   const [participationListData, setParticiPationListData] = useState([]);
   const [tempParticipantsData,setTempParticipantsData] = useState([]);
+
+  const { user } = useFetchUserToken();
+  
   useEffect(() => {
       const fetchUnionData = async () => {
-        const {data:{data}} = await API.get.union_detail(18);
+        console.log("user", user)
+        const { data } = await API.get.unionDetail(user.id);
+        console.log("unionDetail data", data);
         setUnionData(data.union_info);
         setPostData(data.post_info);
         setParticiPationListData(data.union_info.participants);
@@ -22,8 +32,8 @@ const AssociationManagePage = () => {
       fetchUnionData();  
     }, []);
   
-  alert("본페이지는 예시페이지입니다 마무리가 늦어져서 죄송합니다")
-  
+  // alert("본페이지는 예시페이지입니다 마무리가 늦어져서 죄송합니다")
+  if (!!!user) return <></>;
   return (
     <Responsive level={2}>
       <UnionInfo unionData={unionData}/>
