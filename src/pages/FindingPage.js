3857a7405e03e87c13130c2d8f95a958d31b7aaa
NaLeum <wognsdl@likelion.org>
2022-01-31 22:06:44 +0900

    feat: landing 수정

--- src/pages/FindingPage.js
+++ src/pages/FindingPage.js
@@ -13,225 +13,263 @@ import MoreInfoUnion from 'components/Modal/MoreInfoUnion';
 
 import Filter from 'components/common/Filter';
 
-import useFetchUserToken from "hooks/useFetchUserToken";
+import useFetchUserToken from 'hooks/useFetchUserToken';
 import API from 'lib/api';
 
 const FindingPage = ({ location }) => {
-	const query = qs.parse(location.search, { ignoreQueryPrefix: true });
+  const query = qs.parse(location.search, { ignoreQueryPrefix: true });
   const [modalContents, setModalContents] = useState({});
-	const [filterVisible, setFilterVisible] = useState(false);
-	const [filterValue, setFilterValue] = useState({
-		"waiting-people": {}, 
-		"waiting-unions": {}
-	});
-	const [categories, setCategories] = useState([]);
-	const [dataLength, setDataLength] = useState();
+  const [filterVisible, setFilterVisible] = useState(false);
+  const [filterValue, setFilterValue] = useState({
+    'waiting-people': {},
+    'waiting-unions': {},
+  });
+  const [categories, setCategories] = useState([]);
+  const [dataLength, setDataLength] = useState();
   const [isModalActive, setIsModalActive] = useState(true);
-	const $mainRef = useRef(), $modalRef = useRef();
+  const $mainRef = useRef(),
+    $modalRef = useRef();
   const { user } = useFetchUserToken();
-  
-	useEffect(() => {
-    if (query.mode === "waiting-people" && user && user.career.length !== 0 && user.education.length !== 0) {
+
+  useEffect(() => {
+    if (
+      query.mode === 'waiting-people' &&
+      user &&
+      user.career.length !== 0 &&
+      user.education.length !== 0
+    ) {
       setIsModalActive(false);
     } else {
-			setIsModalActive(true);
-		}
-  }, [user, query.mode])
+      setIsModalActive(true);
+    }
+  }, [user, query.mode]);
 
-	useEffect(() => {
+  useEffect(() => {
     if (!$modalRef.current) return;
-    if (Object.keys(modalContents).length !== 0 ) {
-			document.querySelector('body').style.overflow = 'hidden';
-			$modalRef.current.style.display = 'flex';
-		} else {
+    if (Object.keys(modalContents).length !== 0) {
+      document.querySelector('body').style.overflow = 'hidden';
+      $modalRef.current.style.display = 'flex';
+    } else {
       $modalRef.current.style.display = 'none';
     }
-	}, [$modalRef, modalContents]);
-	
-	useEffect(()=>{
-		(async()=>{
-			const fetchCategories = await API.get.all_categories();			
-			setCategories(fetchCategories.data)
-		})();
-	},[])
-	
-  const modalSectionSelector = (current='waiting-people') => {
+  }, [$modalRef, modalContents]);
+
+  useEffect(() => {
+    (async () => {
+      const fetchCategories = await API.get.all_categories();
+      setCategories(fetchCategories.data);
+    })();
+  }, []);
+
+  const modalSectionSelector = (current = 'waiting-people') => {
     switch (current) {
-			case 'waiting-people':
-        return <MoreInfoPerson idx={modalContents.idx} $dom={modalContents.$card} toggleModal={toggleModal} />
-        // api 경력 조회 프로세스 만든 후
-        // return isLogin() 
-        // ? <WaitingInfo info={modalContents.info} idx={modalContents.idx} toggleModal={toggleModal} />
-        // : <MoreInfoPerson idx={modalContents.idx} $dom={modalContents.$card} toggleModal={toggleModal} />
-        
-			case 'waiting-unions':
-        return <MoreInfoUnion toggleModal={toggleModal} />
-        // api 경력 조회 프로세스 만든 후
-        // return isLogin() 
-        // ? <WaitingUnions info={modalContents.info} idx={modalContents.idx} toggleModal={toggleModal} />
-        // : <MoreInfoUnion toggleModal={toggleModal} />
-			default:
-				return <div style={{ width: '100%' }}> 상단의 메뉴를 선택해주세요 </div>;
-		}
-  }
+      case 'waiting-people':
+        return (
+          <MoreInfoPerson
+            idx={modalContents.idx}
+            $dom={modalContents.$card}
+            toggleModal={toggleModal}
+          />
+        );
+      // api 경력 조회 프로세스 만든 후
+      // return isLogin()
+      // ? <WaitingInfo info={modalContents.info} idx={modalContents.idx} toggleModal={toggleModal} />
+      // : <MoreInfoPerson idx={modalContents.idx} $dom={modalContents.$card} toggleModal={toggleModal} />
 
-	const mainSectionSelector = (current='waiting-people') => {
-		
-		switch (current) {
-			case 'waiting-people':
-				return <WaitingPeople openModal={toggleModal} filterValue={filterValue} setDataLength={setDataLength} />;
-			case 'waiting-unions':
-			  return <WaitingUnions openModal={toggleModal} />;
-			default:
-				return <div style={{ width: '100%' }}> 상단의 메뉴를 선택해주세요 </div>;
-		}
-	};
+      case 'waiting-unions':
+        return <MoreInfoUnion toggleModal={toggleModal} />;
+      // api 경력 조회 프로세스 만든 후
+      // return isLogin()
+      // ? <WaitingUnions info={modalContents.info} idx={modalContents.idx} toggleModal={toggleModal} />
+      // : <MoreInfoUnion toggleModal={toggleModal} />
+      default:
+        return (
+          <div style={{ width: '100%' }}> 상단의 메뉴를 선택해주세요 </div>
+        );
+    }
+  };
 
-	const toggleModal = (cardObj) => {
+  const mainSectionSelector = (current = 'waiting-people') => {
+    switch (current) {
+      case 'waiting-people':
+        return (
+          <WaitingPeople
+            openModal={toggleModal}
+            filterValue={filterValue}
+            setDataLength={setDataLength}
+          />
+        );
+      case 'waiting-unions':
+        return <WaitingUnions openModal={toggleModal} />;
+      default:
+        return (
+          <div style={{ width: '100%' }}> 상단의 메뉴를 선택해주세요 </div>
+        );
+    }
+  };
+
+  const toggleModal = (cardObj) => {
     if (!$modalRef.current) return;
     // 모달 닫기
-		if (typeof cardObj === 'boolean' && !cardObj) {
-			document.querySelector('body').style.overflow = '';
-			$modalRef.current.style.display = 'none';
+    if (typeof cardObj === 'boolean' && !cardObj) {
+      document.querySelector('body').style.overflow = '';
+      $modalRef.current.style.display = 'none';
       setModalContents({});
-			return;
-		}
-    
-    // 모달 set
-		if (typeof cardObj.idx === 'number') {
-      setModalContents(cardObj)
-		}
+      return;
+    }
 
-		if (modalContents.idx !== null || cardObj !== null) {
-			document.querySelector('body').style.overflow = 'hidden';
-			$modalRef.current.style.display = 'flex';
-		}
-	};
+    // 모달 set
+    if (typeof cardObj.idx === 'number') {
+      setModalContents(cardObj);
+    }
+
+    if (modalContents.idx !== null || cardObj !== null) {
+      document.querySelector('body').style.overflow = 'hidden';
+      $modalRef.current.style.display = 'flex';
+    }
+  };
 
   if (!user) return <></>;
-	return (
-		<>
-			<FindingHeader current={query.mode ?? 'waiting-people'} />
-			<FindingPagePosition className="FindingPage">
-				<MainSectionPosition>
-					<MainSection ref={$mainRef}>{mainSectionSelector(query.mode)}</MainSection>
-				</MainSectionPosition>
-				<SideSectionPosition filterVisible={filterVisible}>
-					<Filter setFilterVisible={setFilterVisible} filterVisible={filterVisible} mode={query.mode} filterValue={filterValue} setFilterValue={setFilterValue} categories={categories} dataLength={dataLength} />
-				</SideSectionPosition>
-			</FindingPagePosition>
-			{/* 경력 등록 체크 */}
-			{isModalActive && 
-				<ModalPosition isModalActive={isModalActive} ref={$modalRef} onClick={() => { toggleModal(false); }} >
-					<Modal onClick={(e) => { e.stopPropagation(); }} >
-						{modalSectionSelector(query.mode)}
-					</Modal>
-				</ModalPosition>
-			}
-		</>
-	);
+  return (
+    <>
+      <FindingHeader current={query.mode ?? 'waiting-people'} />
+      <FindingPagePosition className='FindingPage'>
+        <MainSectionPosition>
+          <MainSection ref={$mainRef}>
+            {mainSectionSelector(query.mode)}
+          </MainSection>
+        </MainSectionPosition>
+        <SideSectionPosition filterVisible={filterVisible}>
+          <Filter
+            setFilterVisible={setFilterVisible}
+            filterVisible={filterVisible}
+            mode={query.mode}
+            filterValue={filterValue}
+            setFilterValue={setFilterValue}
+            categories={categories}
+            dataLength={dataLength}
+          />
+        </SideSectionPosition>
+      </FindingPagePosition>
+      {/* 경력 등록 체크 */}
+      {isModalActive && (
+        <ModalPosition
+          isModalActive={isModalActive}
+          ref={$modalRef}
+          onClick={() => {
+            toggleModal(false);
+          }}
+        >
+          <Modal
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            {modalSectionSelector(query.mode)}
+          </Modal>
+        </ModalPosition>
+      )}
+    </>
+  );
 };
 
 const FindingPagePosition = styled(Responsive)`
-	position: relative;
-	max-width: 1440px;
-	margin: 0 auto;
-	display: flex;
-	padding-right:0;
-	padding-left:0;
+  position: relative;
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  padding-right: 0;
+  padding-left: 0;
 `;
 
 const MainSectionPosition = styled.div`
-	width: 100%;
-	max-width:1009px;
-	height: 100%;
-	margin-left:auto;
+  width: 100%;
+  max-width: 1009px;
+  height: 100%;
+  margin-left: auto;
 
-	display:flex;
-	justify-content:center;
-	align-items:center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const MainSection = styled.main`
-	width: 100%;
-	height: 100%;
-	padding: 2rem 1rem 2rem 1rem;
-	display: inline-grid;
-	grid-template-columns: repeat(3,minmax(297px,329px));
-	grid-template-rows: 1fr 1fr 1fr;
-	gap: 36px 27px;
-	/* @media screen and (max-width:1435px){
+  width: 100%;
+  height: 100%;
+  padding: 2rem 1rem 2rem 1rem;
+  display: inline-grid;
+  grid-template-columns: repeat(3, minmax(297px, 329px));
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 36px 27px;
+  /* @media screen and (max-width:1435px){
 		grid-template-columns: repeat(auto-fill,minmax(297px,319px));
 	} */
-	@media screen and (max-width:1270px){
-		grid-template-columns: repeat(auto-fill,minmax(297px,329px));
-	} 
+  @media screen and (max-width: 1270px) {
+    grid-template-columns: repeat(auto-fill, minmax(297px, 329px));
+  }
 `;
 
 const ModalPosition = styled.div`
-	width: 100vw;
-	height: 100vh;
-	position: fixed;
-	top: 0;
-	left: 0;
-	right:0;
-	bottom:0;
-	z-index: 2;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
 
-	background-color: rgba(255, 255, 255, 0.5);
-	display: none;
-	justify-content: center;
-	align-items: center;
+  background-color: rgba(255, 255, 255, 0.5);
+  display: none;
+  justify-content: center;
+  align-items: center;
 
-  ${({isModalActive}) => isModalActive && css`
-    display: flex;
-  `}
+  ${({ isModalActive }) =>
+    isModalActive &&
+    css`
+      display: flex;
+    `}
 `;
-
 
 const Modal = styled.div`
-	width: 30vw;
-	min-width: 720px;
-	min-height: 426px;
-	border-radius:10px;
-	background-color: white;
-	box-shadow: 0 5px 7px -1px gray;
+  width: 30vw;
+  min-width: 720px;
+  min-height: 426px;
+  border-radius: 10px;
+  background-color: white;
+  box-shadow: 0 5px 7px -1px gray;
+  z-index: 999;
 
-	z-index: 3;
+  @media screen and (max-width: 720px) {
+    width: 100%;
+    min-width: 0;
 
-	@media screen and (max-width:720px){
-		width:100%;
-		min-width: 0;
-
-		margin:0 1rem;
-	}
+    margin: 0 1rem;
+  }
 `;
 const SideSectionPosition = styled.aside`
-	width: 267px;
-	min-height: calc(100vh - 8rem);
-	position: sticky;
-	top: 4rem;
-	overflow:scroll;
+  width: 267px;
+  min-height: calc(100vh - 8rem);
+  position: sticky;
+  top: 4rem;
+  overflow: scroll;
 
-	${props =>
-    props.filterVisible ?
-    css`
-		border-left: 1px solid #C4C4C4;
-		box-shadow: -2px 0px 3px  rgba(0, 0, 0, 0.1);
-    ` 
-	:
-	css`
-		display:flex;
-		align-items : flex-start;
-		justify-content : center;
-		margin-top:35px;
-	`
-	}
+  ${(props) =>
+    props.filterVisible
+      ? css`
+          border-left: 1px solid #c4c4c4;
+          box-shadow: -2px 0px 3px rgba(0, 0, 0, 0.1);
+        `
+      : css`
+          display: flex;
+          align-items: flex-start;
+          justify-content: center;
+          margin-top: 35px;
+        `}
 
-	@media all and (max-width:500px){
-		display:none;
-	}
-
+  @media all and (max-width:500px) {
+    display: none;
+  }
 `;
 
 export default FindingPage;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,66 +3,64 @@ import Responsive from 'components/common/Responsive';
 import LandingCard from 'components/Landing/LandingCard';
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as LandingLogo } from "../assets/svgs/LandingLogo.svg";
-import { ReactComponent as Person } from "../assets/svgs/Person.svg";
-import { ReactComponent as Company } from "../assets/svgs/Company.svg";
-import JokeImage from '../assets/images/ping.png';
-
-
+import { ReactComponent as LandingLogo } from '../assets/svgs/LandingLogo.svg';
+import { ReactComponent as Person } from '../assets/svgs/Person.svg';
+import { ReactComponent as Company } from '../assets/svgs/Company.svg';
+import JokeImage from '../assets/images/landing.png';
+import JokeImage2 from '../assets/images/landing2.png';
 
 const TopSection = styled.section`
-  width:100%;
-  height:500px;
-  background-color: #49458B;
-  display:flex;
+  width: 100%;
+  height: 500px;
+  background-color: #49458b;
+  display: flex;
   align-items: center;
   justify-content: flex-start;
 `;
 const SectionLayout = styled.div`
-  width:100%;
+  width: 100%;
   max-width: 1300px;
   margin: 0 auto;
-  display:flex;
+  display: flex;
   flex-direction: column;
-  padding:0 100px;
+  padding: 0 100px;
 `;
 const TopCardsSection = styled.section`
-  display:grid;
+  display: grid;
   justify-items: center;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 1rem;
   margin: 0 auto;
   max-width: 1440px;
-  left: 50%; 
+  left: 50%;
   margin-top: -75px;
   padding: 0 75px;
 
-	/* @media (max-width: 1140px) {
+  /* @media (max-width: 1140px) {
     grid-template-columns: 1fr 1fr;
 	} */
   @media (max-width: 1040px) {
     margin-top: -75px;
-
-	}
+  }
+  @media (max-width: 593px) {
+    margin-top: 20px;
+  }
   @media (max-width: 915px) {
     grid-template-columns: 1fr 1fr;
-
-	}
+  }
   @media (max-width: 615px) {
     grid-template-columns: 1fr;
-
-	}
+  }
 `;
 const Title = styled.span`
-  display:block;
+  display: block;
   font-weight: bold;
-  font-size: 32px;  
+  font-size: 32px;
   color: #ffffff;
   text-align: left;
   margin-top: 30px;
   margin-bottom: 30px;
-  word-break:keep-all;
-
+  word-break: keep-all;
 `;
 const Summary = styled.p`
   font-size: 24px;
@@ -70,39 +68,37 @@ const Summary = styled.p`
   text-align: left;
   color: #ffffff;
   font-weight: 500;
-  word-break:keep-all;
+  word-break: keep-all;
 `;
 const MiddleSection1 = styled.section`
   padding-top: 50px;
 `;
 const SectionTitle = styled.span`
-  display:block;
+  display: block;
   font-size: 32px;
   font-weight: 700;
   margin-bottom: 20px;
 `;
 const SectionSummary = styled(Summary)`
-  font-size:24px;
+  font-size: 24px;
   color: #494949;
 `;
 const ImageSectionOuterLayout = styled.div`
-  display:flex;
-  align-items:center;
+  display: flex;
+  align-items: center;
   justify-content: center;
-  margin-top:30px;
-  margin-bottom: 50px;
-  padding: 0 120px;
-  gap:30px;
+  margin-top: 50px;
+  margin-bottom: 100px;
 `;
 const ImageSectionInnerLayout = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
-  gap:30px;
+  gap: 30px;
 `;
 const ImageItem = styled.article`
-  width:150px;
-  height:150px;
-  display:flex;
+  width: 150px;
+  height: 150px;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -114,66 +110,67 @@ const ArticleTitle = styled.h4`
   font-size: 18px;
   color: #767676;
   line-height: 24px;
-  margin-bottom:14px;
+  margin-bottom: 14px;
 `;
 const ImageItemBrown = styled(ImageItem)`
   background: #767676;
-  border:0;
+  border: 0;
   border-radius: 4px;
 `;
 const ArticleTitleWhite = styled(ArticleTitle)`
-  color:white;
+  color: white;
   font-weight: 500;
 `;
 const ServiceImageLayout = styled.div`
-  width:100%;
-  height:322px;
-  border: 1px solid #C4C4C4;
-  display:flex;
+  width: 100%;
+  display: flex;
   justify-content: center;
   align-items: center;
 `;
 const MiddleSection2 = styled.section`
-  width:100%;
+  width: 100%;
   padding-bottom: 95px;
 `;
 
 const MiddleSection3 = styled(TopSection)`
-  width:100%;
-  height:auto;
-  padding-top:50px;
-  padding-bottom:50px;
+  width: 100%;
+  height: auto;
+  padding-top: 50px;
+  padding-bottom: 50px;
 `;
 const Section3Title = styled(SectionTitle)`
-  margin-top:0;
-  color:#fff;
+  margin-top: 0;
+  color: #fff;
 `;
 const Section3Summary = styled(SectionSummary)`
   color: #fff;
   font-weight: 500;
 `;
 const ImageSectionOuterLayoutNoGap = styled(ImageSectionOuterLayout)`
-  gap:0;
+  gap: 0;
 `;
 const MiddleSection4 = styled(MiddleSection3)`
   background-color: #fff;
 `;
 const Section4Title = styled(Section3Title)`
-  color: #49458B;
+  color: #49458b;
 `;
 const Section4Summary = styled(SectionSummary)`
-  color: #49458B;  
+  color: #49458b;
   font-weight: 500;
-  margin-bottom:70px;
+  margin-bottom: 70px;
 `;
 const CardContainer = styled.section`
-  display:flex;
+  display: flex;
   align-items: stretch;
-  justify-content: space-between;
-  gap:20px;
+  justify-content: center;
+  flex-wrap: wrap;
+  section {
+    margin: 5px;
+  }
 `;
 const SummaryCard = styled.section`
-  background-color: #49458B;  
+  background-color: #49458b;
   width: 350px;
   min-height: 380px;
   padding-top: 40px;
@@ -181,35 +178,35 @@ const SummaryCard = styled.section`
   word-break: keep-all;
 `;
 const CardTitle = styled.span`
-  display:block;
-  text-align:center;
+  display: block;
+  text-align: center;
   font-weight: 500;
   font-size: 24px;
-  color: #FFFFFF;
-  margin-bottom:50px;
+  color: #ffffff;
+  margin-bottom: 50px;
 `;
 const CardSummary = styled.span`
-  display:block;
+  display: block;
   font-size: 18px;
   line-height: 28px;
   font-weight: 300;
   text-align: center;
-  color: #FFFFFF;
-  & > span{
+  color: #ffffff;
+  & > span {
     font-weight: 500;
   }
 `;
 const MiddleSection5 = styled(TopSection)`
-  background: #F2F2F2;
-  width:100%;
-  height:auto;
-  padding-top:50px;
-  padding-bottom:50px;
+  background: #f2f2f2;
+  width: 100%;
+  height: auto;
+  padding-top: 50px;
+  padding-bottom: 50px;
   flex-direction: column;
 `;
 const QustionTitle = styled(SectionTitle)`
-  margin-top:0;
-  color: #49458B;
+  margin-top: 0;
+  color: #49458b;
 `;
 const LandingPage = () => {
   return (
@@ -219,84 +216,66 @@ const LandingPage = () => {
           <LandingLogo />
           <Title> 개인투자 조합 결성 관리 플랫폼 </Title>
           <Summary>
-          UNIFOLIO는 출자자와 운용사의 개인 투자 조합 결성을 중개하고, <br/>
-          결성된 조합의 관리를 돕는 플랫폼 서비스입니다.
+            UNIFOLIO는 출자자와 운용사의 개인 투자 조합 결성을 중개하고, <br />
+            결성된 조합의 관리를 돕는 플랫폼 서비스입니다.
           </Summary>
         </SectionLayout>
       </TopSection>
       <TopCardsSection>
-          <LandingCard title="출자 대기중인 출자자" number="000"/>
-          <LandingCard title="결성 대기중인 개인투자조합" number="000"/>
-          <LandingCard title="결성 완료된 개인투자조합" number="000"/>
-        </TopCardsSection>
-        <MiddleSection1>
-          <SectionLayout>
-            <SectionTitle>
-              서로를 알아보는 방법.
-            </SectionTitle>
-            <SectionSummary>
-              출자자는 본인의 경력을 운용사는 투자 이력을 등록하고 공개하며,<br/>
-              출자자와 운용사는 공개된 정보를 통해 서로에 대해 알 수 있습니다.
-            </SectionSummary>
-            <ImageSectionOuterLayout>
-              <ImageSectionInnerLayout>
-                <ImageItem>
-                  <ArticleTitle>
-                    출자자
-                  </ArticleTitle>
-                  <Person />
-                </ImageItem>
-                <ImageItemBrown>
-                  <ArticleTitleWhite>
-                    운용사
-                  </ArticleTitleWhite>
-                  <Company />
-                </ImageItemBrown>
-              </ImageSectionInnerLayout>
-              <ServiceImageLayout>
-                  <img src={JokeImage}/>
-              </ServiceImageLayout>
-            </ImageSectionOuterLayout>
-          </SectionLayout>
-        </MiddleSection1>
+        <LandingCard title='출자 대기중인 출자자' number='000' />
+        <LandingCard title='결성 대기중인 개인투자조합' number='000' />
+        <LandingCard title='결성 완료된 개인투자조합' number='000' />
+      </TopCardsSection>
+      <MiddleSection1>
+        <SectionLayout>
+          <SectionTitle>서로를 알아보는 방법.</SectionTitle>
+          <SectionSummary>
+            출자자는 본인의 경력을 운용사는 투자 이력을 등록하고 공개하며,
+            <br />
+            출자자와 운용사는 공개된 정보를 통해 서로에 대해 알 수 있습니다.
+          </SectionSummary>
+          <ImageSectionOuterLayout>
+            <ServiceImageLayout>
+              <img src={JokeImage} style={{ width: '100%' }} />
+            </ServiceImageLayout>
+          </ImageSectionOuterLayout>
+        </SectionLayout>
+      </MiddleSection1>
       <MiddleSection3>
         <SectionLayout>
-          <Section3Title>
-            쉽고 간편한 조합 결성.
-          </Section3Title>
+          <Section3Title>쉽고 간편한 조합 결성.</Section3Title>
           <Section3Summary>
-            많고 복잡한 서류 작성은 이제 그만.<br/> 
-            운용사와 출자자가 전부 모이면 조합 결성에 필요한 서류를 시스템에서 자동으로 제공합니다.
+            많고 복잡한 서류 작성은 이제 그만.
+            <br />
+            운용사와 출자자가 전부 모이면 조합 결성에 필요한 서류를 시스템에서
+            자동으로 제공합니다.
           </Section3Summary>
           <ImageSectionOuterLayoutNoGap>
-            <img src={JokeImage}/>
-            <img src={JokeImage}/>
-            <img src={JokeImage}/>
+            <img src={JokeImage2} style={{ width: '100%' }} />
           </ImageSectionOuterLayoutNoGap>
         </SectionLayout>
       </MiddleSection3>
       <MiddleSection4>
         <SectionLayout>
-          <Section4Title>
-            새로운 가치 만들기.
-          </Section4Title>
+          <Section4Title>새로운 가치 만들기.</Section4Title>
           <Section4Summary>
-            개인투자조합에 출자하면 소득 공제를 받으며 초기 스타트업에 투자할 수 있습니다.
+            개인투자조합에 출자하면 소득 공제를 받으며 초기 스타트업에 투자할 수
+            있습니다.
           </Section4Summary>
           <CardContainer>
             <SummaryCard>
               <CardTitle>개인투자 조합이란,</CardTitle>
               <CardSummary>
-                개인투자조합이란 벤처기업육성에 관한 
-                <br/>
+                개인투자조합이란 벤처기업육성에 관한
+                <br />
                 특별조치법에 의해 설립된 조합으로
-                <br/>
+                <br />
                 중소벤처기업부에 등록된 벤처기업과
-                <br/>
+                <br />
                 창업자에 투자할 목적으로
-                <br/>
+                <br />
                 개인이나 조합이 출자하여 결성하는
-                <br/>
+                <br />
                 조합을 의미합니다.
               </CardSummary>
             </SummaryCard>
@@ -304,11 +283,11 @@ const LandingPage = () => {
               <CardTitle>개인투자 조합의 결성은,</CardTitle>
               <CardSummary>
                 출자총액 1억원 이상
-                <br/>
+                <br />
                 조합원수 49인 이하
-                <br/>
+                <br />
                 업무집행조합원의 출자 지분 5% 이상
-                <br/>
+                <br />
                 등의 요건이 필요합니다.
               </CardSummary>
             </SummaryCard>
@@ -316,31 +295,29 @@ const LandingPage = () => {
               <CardTitle> 개인투자 조합의 구성은,</CardTitle>
               <CardSummary>
                 <span>출자자 (LP, Limited Partner)와</span>
-                <br/>
-                <span>
-                  운용사(GP, General Partener)가
-                </span> 있으며
-                <br/>
+                <br />
+                <span>운용사(GP, General Partener)가</span> 있으며
+                <br />
                 출자자는 조합에 자금을 출자해 운용사에
-                <br/>
+                <br />
                 맡기고 그 대가로 보수를 지급하며
-                <br/>
+                <br />
                 운용사는 조합의 자금을 운용해 투자를
-                <br/>
+                <br />
                 집행하고 그 대가로 보수를 획득합니다.
               </CardSummary>
             </SummaryCard>
           </CardContainer>
         </SectionLayout>
       </MiddleSection4>
-        <MiddleSection5>
-          <SectionLayout>
-            <QustionTitle>Question</QustionTitle>
-            <Accordion />
-          </SectionLayout>
-        </MiddleSection5>
+      <MiddleSection5>
+        <SectionLayout>
+          <QustionTitle>Question</QustionTitle>
+          <Accordion />
+        </SectionLayout>
+      </MiddleSection5>
     </>
   );
-}
+};
 
 export default LandingPage;
