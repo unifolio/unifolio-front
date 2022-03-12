0158793139a3214ff866d09d4cb5cb0ef1af96e2
ppamppamman <ryou9305@naver.com>
2022-03-12 15:43:34 +0900

    fix: 주민등록번호 rrn의 작대기 validation에 따른 제거 로직 추가

--- src/composition/Profile/DefaultInfoBusiness.js
+++ src/composition/Profile/DefaultInfoBusiness.js
@@ -1,112 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import styled, { css } from 'styled-components';
-import styles from 'lib/styles';
-import useDaumPostcode from 'hooks/useDaumPostcode';
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
+import styles from "lib/styles";
+import useDaumPostcode from "hooks/useDaumPostcode";
 
 const DefaultInfoBusiness = ({ user, handleSubmit }) => {
   const [isModifing, setIsModifing] = useState(false);
-  const [formData, setFormData] = useState({...user});
-  const { handleClickToChangeAddress } = useDaumPostcode(callbackCompleteSearchPostcodeProcess)
+  const [formData, setFormData] = useState({ ...user });
+  const { handleClickToChangeAddress } = useDaumPostcode(
+    callbackCompleteSearchPostcodeProcess
+  );
   const informations = [
-    {labelKr: "법인 이름", labelEn: "corporate_name", value:user.corporate_name},
-    {labelKr: "이메일", labelEn: "email", value:user.email},
-    {labelKr: "사업자 등록번호", labelEn: "corporate_registration", value:user.corporate_registration},
-    {labelKr: "법인 등록번호", labelEn: "company_registration_number", value:user.company_registration_number},
+    {
+      labelKr: "법인 이름",
+      labelEn: "corporate_name",
+      value: user.corporate_name,
+    },
+    { labelKr: "이메일", labelEn: "email", value: user.email },
+    {
+      labelKr: "사업자 등록번호",
+      labelEn: "corporate_registration",
+      value: user.corporate_registration,
+    },
+    {
+      labelKr: "법인 등록번호",
+      labelEn: "company_registration_number",
+      value: user.company_registration_number,
+    },
     // {labelKr: "연락처", labelEn: "phone_number", value:user.phone_number ?? "연락처 입력이 필요합니다"},
-    {labelKr: "우편번호", labelEn: "address_postcode_business", value:user.address_postcode_business},
-    {labelKr: "사무실 주소", labelEn: "address_business", value:user.address_business},
-    {labelKr: "사무실 상세 주소", labelEn: "address_detail_business", value:user.address_detail_business},
-    {labelKr: "법인 설명", labelEn: "introduction", value:user.introduction}
+    {
+      labelKr: "우편번호",
+      labelEn: "address_postcode_business",
+      value: user.address_postcode_business,
+    },
+    {
+      labelKr: "사무실 주소",
+      labelEn: "address_business",
+      value: user.address_business,
+    },
+    {
+      labelKr: "사무실 상세 주소",
+      labelEn: "address_detail_business",
+      value: user.address_detail_business,
+    },
+    { labelKr: "법인 설명", labelEn: "introduction", value: user.introduction },
   ];
-  
+
   function callbackCompleteSearchPostcodeProcess(data) {
-    setFormData({...formData, address_business: data.address, address_postcode_business: data.zonecode });
+    setFormData({
+      ...formData,
+      address_business: data.address,
+      address_postcode_business: data.zonecode,
+    });
   }
 
   const handleClickModifyButton = () => {
     setIsModifing(!isModifing);
     if (!isModifing) {
-      setFormData({...user});
+      setFormData({ ...user });
     } else {
       handleSubmit(formData);
     }
-  }
-  const handleInputChange = ({name, value}) => {
-    const newFormData = {...formData, [name]: value }
+  };
+  const handleInputChange = ({ name, value }) => {
+    const newFormData = { ...formData, [name]: value };
     setFormData(newFormData);
-  }
+  };
 
-	return (
-		<DefaultInfoLayout>
-			<DefaultInfoHeader>
+  return (
+    <DefaultInfoLayout>
+      <DefaultInfoHeader>
         <DefaultInfoTitle> 회원 정보 </DefaultInfoTitle>
-        <DefaultInfoModifyButton isModifing={isModifing} onClick={handleClickModifyButton} />
-        {isModifing && <DefaultInfoModifyCancelButton onClick={() => setIsModifing(false)} /> }
+        <DefaultInfoModifyButton
+          isModifing={isModifing}
+          onClick={handleClickModifyButton}
+        />
+        {isModifing && (
+          <DefaultInfoModifyCancelButton onClick={() => setIsModifing(false)} />
+        )}
       </DefaultInfoHeader>
-			<HeadlineBottomBorder />
-      {!isModifing
-      ? <InfoPresenter>
-          {informations.map(({labelKr, labelEn, value}) => (
+      <HeadlineBottomBorder />
+      {!isModifing ? (
+        <InfoPresenter>
+          {informations.map(({ labelKr, labelEn, value }) => (
             <InfoRow key={`${labelEn}-modify`}>
               <InfoLabel>{labelKr}</InfoLabel>
-              <InfoContents>{value ?? `${labelKr} 입력이 필요합니다.`}</InfoContents>
+              {value ? (
+                <InfoContents> {value} </InfoContents>
+              ) : (
+                <InfoContents color={"red"}>
+                  {`${labelKr} 입력이 필요합니다.`}
+                </InfoContents>
+              )}
             </InfoRow>
           ))}
         </InfoPresenter>
-      : <InfoModifier>
-          {informations.map(({labelKr, labelEn}) => {
-            if (labelEn === "address_business" || labelEn === "address_postcode_business") {
+      ) : (
+        <InfoModifier>
+          {informations.map(({ labelKr, labelEn }) => {
+            if (
+              labelEn === "address_business" ||
+              labelEn === "address_postcode_business"
+            ) {
               return (
                 <InfoRow key={`${labelEn}-modify`}>
                   <InfoLabel>{labelKr}</InfoLabel>
-                  <DefaultInfoInput 
-                    readonly={true} placeHolder={user[labelEn] ?? ""}
+                  <DefaultInfoInput
+                    readonly={true}
+                    placeHolder={user[labelEn] ?? ""}
                     value={formData[labelEn] ?? ""}
                     onClick={handleClickToChangeAddress}
                   />
                 </InfoRow>
-              )
+              );
             } else {
               return (
                 <InfoRow key={`${labelEn}-modify`}>
                   <InfoLabel>{labelKr}</InfoLabel>
-                  <DefaultInfoInput 
-                    placeHolder={user[labelEn]} 
+                  <DefaultInfoInput
+                    placeHolder={user[labelEn]}
                     value={formData[labelEn] ?? ""}
-                    onChange={(e) => {handleInputChange({name:labelEn, value:e.target.value})}} 
+                    onChange={(e) => {
+                      handleInputChange({
+                        name: labelEn,
+                        value: e.target.value,
+                      });
+                    }}
                   />
                 </InfoRow>
-              )
+              );
             }
           })}
         </InfoModifier>
-      }
-		</DefaultInfoLayout>
-	);
+      )}
+    </DefaultInfoLayout>
+  );
 };
 
 const DefaultInfoLayout = styled.section`
-	margin-top: 3rem;
-	
+  margin-top: 3rem;
+
   display: flex;
-	flex-direction: column;
-	
-	& + section {
-		margin-top: 5rem;
-	}
+  flex-direction: column;
+
+  & + section {
+    margin-top: 5rem;
+  }
 `;
 
 const DefaultInfoHeader = styled.header`
-  
   display: flex;
   flex-direction: row;
   align-items: center;
-`
+`;
 
 const DefaultInfoTitle = styled.span`
   font-size: var(--fontSize24);
   font-weight: bold;
-`
+`;
 const DefaultInfoModifyButton = styled.button`
   width: 65px;
   height: 80%;
@@ -114,42 +164,42 @@ const DefaultInfoModifyButton = styled.button`
   border: 1px solid;
   border-radius: 30px;
   cursor: pointer;
-  
-  ${(props) => !props.isModifing
-    ? css`
-        color: ${styles.palette.unifolioBlue};
-        background-color: white;
-        ::after {
-          content: "수정"
-        }
-      `
-    : css`
-        color: white;
-        background-color: ${styles.palette.unifolioBlue};
-        ::after {
-          content: "저장"
-        }
-      `
-  }
+
+  ${(props) =>
+    !props.isModifing
+      ? css`
+          color: ${styles.palette.unifolioBlue};
+          background-color: white;
+          ::after {
+            content: "수정";
+          }
+        `
+      : css`
+          color: white;
+          background-color: ${styles.palette.unifolioBlue};
+          ::after {
+            content: "저장";
+          }
+        `}
 `;
 const DefaultInfoModifyCancelButton = styled.button`
   width: 65px;
   height: 80%;
   margin-left: 15px;
-  
+
   background-color: white;
   border: 1px solid black;
   border-radius: 30px;
   cursor: pointer;
 
   ::after {
-    content: "취소"
+    content: "취소";
   }
 `;
 
 const HeadlineBottomBorder = styled.div`
-	border-bottom: 2px solid;
-	margin-bottom: 2rem;
+  border-bottom: 2px solid;
+  margin-bottom: 2rem;
 `;
 
 const InfoPresenter = styled.div``;
@@ -157,7 +207,7 @@ const InfoModifier = styled.div``;
 
 const InfoRow = styled.div`
   display: flex;
-  align-items:center;
+  align-items: center;
 
   & + & {
     margin-top: 22px;
@@ -166,17 +216,25 @@ const InfoRow = styled.div`
 
 const InfoLabel = styled.div`
   color: grey;
-  
+
   display: flex;
   flex-basis: 195px;
-`
+`;
 
 const InfoContents = styled.div`
   font-size: var(--fontSize20);
+  ${({ color }) =>
+    color &&
+    css`
+      color: ${color};
+    `}
 `;
 
 const DefaultInfoInput = styled.input.attrs((props) => ({
-  type: "text", placeholder: props.placeHolder, id: props.id, readOnly: props.readonly
+  type: "text",
+  placeholder: props.placeHolder,
+  id: props.id,
+  readOnly: props.readonly,
 }))`
   padding: 0;
   border: none;
