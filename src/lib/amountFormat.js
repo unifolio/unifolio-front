9bb3fe104c847b169b3186d9831f98fe070f1dcf
ppamppamman <ryou9305@naver.com>
2022-03-26 22:15:57 +0900

    add: amountFormatingMultiply 함수 추가

--- src/lib/amountFormat.js
+++ src/lib/amountFormat.js
@@ -1,3 +1,8 @@
 export function amountFormating(amount){
     return amount/10000 >= 10000 ? amount/100000000+"억원":amount/10000+"만원";
 }
+
+export function amountFormatingMultiply(amount){
+    const inputAmount = amount*1000000
+    return inputAmount/10000 >= 10000 ? inputAmount/100000000+"억원":inputAmount/10000+"만원";
+}
